d04927a02b2347541d086fb46e19f68e1d763ad7
JHB <16675200+jharveyb@users.noreply.github.com>
2025-09-22 12:20:43 -0400

    Unify ldk-node dep., add tool to analyze node_announcement timings

--- gossip_analyze/src/main.rs
+++ gossip_analyze/src/main.rs
@@ -93,6 +93,7 @@ enum Commands {
         peers: Vec<String>,
     },
     FetchAddresses {},
+    ParseAddresses {},
 }
 
 #[tokio::main]
@@ -125,12 +126,15 @@ async fn main() -> anyhow::Result<()> {
                     .await?
                     .json::<serde_json::Value>()
                     .await?;
+                let last_msg = res["last_update"]
+                    .as_u64()
+                    .ok_or_else(|| anyhow!("No last update for {}", node))?;
                 let addrs = res["addresses"]
                     .as_array()
                     .ok_or_else(|| anyhow!("No addresses for {}", node))?;
                 for addr in addrs {
                     let network_addr = addr["addr"].as_str().unwrap();
-                    let connection_info = format!("{}@{}\n", node, network_addr);
+                    let connection_info = format!("{}@{},{}\n", node, network_addr, last_msg);
                     buf_node_addresses.write_all(connection_info.as_bytes())?;
                 }
                 buf_node_addresses.flush()?;
@@ -144,6 +148,96 @@ async fn main() -> anyhow::Result<()> {
                 delay_padding = std::time::Duration::from_millis(1005 - (loop_delay as u64));
                 sleep(delay_padding).await;
             }
+
+            // TODO: add step that splits file to clearnet and tor
+            // also filters out localhost
+
+            Ok(())
+        }
+        Commands::ParseAddresses {} => {
+            // load node list from file
+            let datadir = "./gossip_dump";
+            let node_addresses_path = format!("{}/{}", datadir, "node_addresses.txt");
+            let node_addresses_file = fs::read_to_string(node_addresses_path)?;
+            let mut node_addresses = node_addresses_file
+                .lines()
+                .map(String::from)
+                .collect::<Vec<_>>();
+
+            println!("Loaded {} addresses", node_addresses.len());
+
+            let now = chrono::Utc::now().timestamp();
+            let mut last_hour = HashMap::new();
+            let mut last_day = HashMap::new();
+            let mut last_week = HashMap::new();
+            let mut last_2weeks = HashMap::new();
+            let mut last_month = HashMap::new();
+            let mut last_3months = HashMap::new();
+            let mut last_6months = HashMap::new();
+            let mut last_year = HashMap::new();
+            let mut other = HashMap::new();
+
+            let hr = 3600;
+            let day = hr * 24;
+            let week = day * 7;
+            let twoweeks = week * 2;
+            let month = week * 4;
+            let threemonths = month * 3;
+            let sixmonths = month * 6;
+            let oneyear = month * 12;
+            for node in node_addresses {
+                let node_ts = node.split(',').next_back().unwrap().parse::<i64>().unwrap();
+                let last_seen = now - node_ts;
+                match last_seen {
+                    x if x <= hr => last_hour.insert(node, node_ts),
+                    x if x <= day => last_day.insert(node, node_ts),
+                    x if x <= week => last_week.insert(node, node_ts),
+                    x if x <= twoweeks => last_2weeks.insert(node, node_ts),
+                    x if x <= month => last_month.insert(node, node_ts),
+                    x if x <= threemonths => last_3months.insert(node, node_ts),
+                    x if x <= sixmonths => last_6months.insert(node, node_ts),
+                    x if x <= oneyear => last_year.insert(node, node_ts),
+                    _ => other.insert(node, node_ts),
+                };
+            }
+
+            println!("Last seen binning:");
+            println!("Last hour: {}", last_hour.len());
+            println!("Last day: {}", last_day.len());
+            println!("Last week: {}", last_week.len());
+            println!("Last 2 weeks: {}", last_2weeks.len());
+            println!("Last month: {}", last_month.len());
+            println!("Last 3 months: {}", last_3months.len());
+            println!("Last 6 months: {}", last_6months.len());
+            println!("Last year: {}", last_year.len());
+            println!("Other: {}", other.len());
+
+            println!("Summed stats:");
+            let mut total = last_hour.len();
+            println!("Last hour: {}", total);
+            total += last_day.len();
+            println!("Last day: {}", total);
+            total += last_week.len();
+            println!("Last week: {}", total);
+            total += last_2weeks.len();
+            println!("Last 2 weeks: {}", total);
+            total += last_month.len();
+
+            let within_month = total;
+            println!("Last month: {}", total);
+            total += last_3months.len();
+            println!("Last 3 months: {}", total);
+            total += last_6months.len();
+            println!("Last 6 months: {}", total);
+            total += last_year.len();
+            println!("Last year: {}", total);
+            total += other.len();
+            println!("Other: {}", total);
+
+            println!(
+                "Percent within last month: {}",
+                (within_month as f64 / total as f64) * 100.0
+            );
 
             Ok(())
         }
@@ -161,8 +255,9 @@ async fn main() -> anyhow::Result<()> {
             // BFX 0 node rejected us, interesting
             let acinq = "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f@3.33.236.230:9735";
             let wos = "035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226@170.75.163.209:9735";
-            let megalithic = "0322d0e43b3d92d30ed187f4e101a9a9605c3ee5fc9721e6dac3ce3d7732fbb13e@164.92.106.32:9735";
-            let default_peers = [acinq, wos, megalithic];
+            let megalithic: &'static str = "0322d0e43b3d92d30ed187f4e101a9a9605c3ee5fc9721e6dac3ce3d7732fbb13e@164.92.106.32:9735";
+            let lqwd_btcpay = "02cc611df622184f8c23639cf51b75001c07af0731f5569e87474ba3cc44f079ee@192.243.215.105:59735";
+            let default_peers = [acinq, wos, megalithic, lqwd_btcpay];
             let default_peers = default_peers
                 .iter()
                 .map(|p| parse_peer_specifier(p))
@@ -208,6 +303,13 @@ async fn main() -> anyhow::Result<()> {
 
             let current_graph = node.network_graph();
             println!("Final graph stats:");
+            println!("Nodes: {:?}", current_graph.list_nodes().len());
+            println!("Channels: {:?}", current_graph.list_channels().len());
+
+            // I think this won't prune one side of a channel that's offline? weird
+            current_graph.prune();
+
+            println!("Final graph stats after pruning:");
             println!("Nodes: {:?}", current_graph.list_nodes().len());
             println!("Channels: {:?}", current_graph.list_channels().len());
 
