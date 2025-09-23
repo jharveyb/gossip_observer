4dc586dc0e3776b5147d8f7f366d458511c9cd87
JHB <16675200+jharveyb@users.noreply.github.com>
2025-09-23 12:11:07 -0400

    analyze: remove deprecated Compare cmd

--- gossip_analyze/src/main.rs
+++ gossip_analyze/src/main.rs
@@ -1,68 +1,17 @@
 use anyhow::anyhow;
 use anyhow::bail;
-use chrono::DateTime;
-use chrono::Utc;
 use clap::{Parser, Subcommand};
-use csv::ReaderBuilder;
 use ldk_node::bitcoin::secp256k1::PublicKey;
 use ldk_node::lightning::ln::msgs::SocketAddress;
-use serde::{Deserialize, Deserializer};
-use statrs::statistics::Statistics;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::fs;
-use std::fs::exists;
 use std::io::BufWriter;
 use std::io::Write;
-use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::time::sleep;
-
-fn deserialize_unix_micros<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let micros = i64::deserialize(deserializer)?;
-    DateTime::from_timestamp_micros(micros)
-        .ok_or_else(|| serde::de::Error::custom("Invalid timestamp microseconds"))
-}
-
-fn compute_basic_stats(data: &[f64]) -> BasicStats {
-    let min = data.min();
-    let max = data.max();
-    let mean = data.mean();
-    let geomean = data.geometric_mean();
-    let std_dev = data.std_dev();
-    BasicStats {
-        min,
-        max,
-        mean,
-        geomean,
-        std_dev,
-    }
-}
-
-// v0 header: unix_micros,msg_hash,peer_pubkey,msg_type,size
-#[derive(Debug, Deserialize)]
-struct GossipRecordV0 {
-    #[serde(deserialize_with = "deserialize_unix_micros")]
-    unix_micros: DateTime<Utc>,
-    msg_hash: String,
-    peer_pubkey: String,
-    msg_type: String,
-    size: u64,
-}
-
-#[derive(Debug)]
-struct BasicStats {
-    min: f64,
-    max: f64,
-    mean: f64,
-    geomean: f64,
-    std_dev: f64,
-}
 
 fn parse_peer_specifier(peer_specifier: &str) -> anyhow::Result<(PublicKey, SocketAddress)> {
     let (pubkey_str, address) = peer_specifier
@@ -84,10 +33,6 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Compare {
-        #[arg(help = "Paths to CSV files to compare", required = true)]
-        paths: Vec<PathBuf>,
-    },
     Dump {
         #[arg(help = "List of peers to sync gossip from", required = false)]
         peers: Vec<String>,
@@ -151,7 +96,6 @@ async fn main() -> anyhow::Result<()> {
 
             // TODO: add step that splits file to clearnet and tor
             // also filters out localhost
-
             Ok(())
         }
         Commands::ParseAddresses {} => {
@@ -255,9 +199,10 @@ async fn main() -> anyhow::Result<()> {
             // BFX 0 node rejected us, interesting
             let acinq = "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f@3.33.236.230:9735";
             let wos = "035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226@170.75.163.209:9735";
-            let megalithic: &'static str = "0322d0e43b3d92d30ed187f4e101a9a9605c3ee5fc9721e6dac3ce3d7732fbb13e@164.92.106.32:9735";
+            let megalithic = "0322d0e43b3d92d30ed187f4e101a9a9605c3ee5fc9721e6dac3ce3d7732fbb13e@164.92.106.32:9735";
             let lqwd_btcpay = "02cc611df622184f8c23639cf51b75001c07af0731f5569e87474ba3cc44f079ee@192.243.215.105:59735";
-            let default_peers = [acinq, wos, megalithic, lqwd_btcpay];
+            let opennode = "028d98b9969fbed53784a36617eb489a59ab6dc9b9d77fcdca9ff55307cd98e3c4@18.222.70.85:9735";
+            let default_peers = [acinq, wos, megalithic, lqwd_btcpay, opennode];
             let default_peers = default_peers
                 .iter()
                 .map(|p| parse_peer_specifier(p))
@@ -338,76 +283,6 @@ async fn main() -> anyhow::Result<()> {
             buf_channel_list.flush()?;
 
             node.stop()?;
-            Ok(())
-        }
-        Commands::Compare { paths } => {
-            if paths.len() < 2 {
-                bail!("Error: At least 2 CSV files are required for comparison");
-            }
-
-            for path in paths {
-                if !exists(path)? {
-                    bail!("File '{}' does not exist", path.display());
-                }
-            }
-
-            println!("Comparing {} files:", paths.len());
-
-            // One reader per file, all readers have the same header.
-            let mut readers = Vec::new();
-            for path in paths {
-                let reader = ReaderBuilder::new().has_headers(false).from_path(path)?;
-                readers.push(reader);
-            }
-
-            // Load CSVs into a map, keyed by the message hash so we can compare
-            // later. Should we sort this in any fancy way?
-            let mut msg_maps = HashMap::new();
-            for mut reader in readers {
-                // Store messages keyed by hash - we mostly want to diff the timing.
-                let mut msgs = HashMap::new();
-                let mut peer_pubkey = None;
-                for record in reader.deserialize() {
-                    let msg: GossipRecordV0 = record?;
-                    if peer_pubkey.is_none() {
-                        peer_pubkey = Some(msg.peer_pubkey.clone());
-                        println!("{peer_pubkey:?}");
-                    }
-                    msgs.insert(msg.msg_hash.clone(), msg);
-                }
-                msg_maps.insert(peer_pubkey.unwrap(), msgs);
-            }
-
-            // Compute timing differences. We'll use the convention:
-            // Negative = other peer received msg before us, postive = delay
-            // in other peer recv'ing message.
-
-            // TODO: remove all these unwraps
-            let mut all_peers = msg_maps.keys().collect::<Vec<_>>();
-            let primary_peer = all_peers.pop().unwrap();
-            let mut timings = HashMap::new();
-            let primary_node_msgs = msg_maps.get(primary_peer).unwrap();
-            let secondary_msgs = msg_maps.get(all_peers.first().unwrap().as_str()).unwrap();
-            for (msg_hash, msg) in primary_node_msgs.iter() {
-                if let Some(secondary_msg) = secondary_msgs.get(msg_hash) {
-                    let shift =
-                        (secondary_msg.unix_micros - msg.unix_micros).num_milliseconds() as i32;
-                    // .unwrap() as i32;
-                    timings.insert(msg_hash.clone(), shift);
-                }
-            }
-
-            println!("Timings found: {}", timings.len());
-
-            let timing_list: Vec<f64> = timings.values().map(|&x| f64::from(x)).collect();
-            let abs_timing_list: Vec<f64> = timings.values().map(|&x| f64::from(x.abs())).collect();
-
-            let timing_stats = compute_basic_stats(&timing_list);
-            let abs_timing_stats = compute_basic_stats(&abs_timing_list);
-
-            println!("Timing stats: {timing_stats:?}");
-            println!("Absolute timing stats: {abs_timing_stats:?}");
-
             Ok(())
         }
     }
