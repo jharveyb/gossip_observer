0c139e6f8ec0ffd8866253916c891f785c8b73f2
JHB <16675200+jharveyb@users.noreply.github.com>
2026-02-04 14:10:54 -0500

    infra: playbook to deploy observer controllers
    Missing our collector to community mapping, but working otherwise.

--- scripts/stage_binaries.sh
+++ scripts/stage_binaries.sh
@@ -6,3 +6,4 @@ just build-prod
 
 cp target/release/gossip_collector infra/ansible/binaries/current/gossip_collector
 cp target/release/gossip_archiver infra/ansible/binaries/current/gossip_archiver
+cp target/release/observer_controller infra/ansible/binaries/current/observer_controller
